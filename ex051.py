d5e8e304fb71a40c82645688bef41ab16366fd81
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-04 22:59:11 -0300

    adicionando

--- ex051.py
+++ ex051.py
@@ -1,4 +1,9 @@
-b=int(input('primeiro termo da p.a:'))
-a=int(input('razão da p.a:'))
-for c in range(b,a*9+b+1,a):
-    print(c,end=' ')
+g=input('com for ou com while:')
+if g=='for':
+    b=int(input('primeiro termo da p.a:'))
+    a=int(input('razão da p.a:'))
+    for c in range(b,a*9+b+1,a):
+        print(c,end=' ')
+elif g=='while':
+    a=int(input('primeiro termo da P.A:'))
+    while
