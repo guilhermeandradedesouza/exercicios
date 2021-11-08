9dc192d21461f27358a7d3cba3eca94f9ab46109
Guilherme Andrade de Souza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-08 16:49:28 -0300

    Update and rename ex028.py to ex028 e 58.py

--- ex028.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-z=b=0
-while b!=randint(0,10):
-    b=int(input('um numero de 0 a 10:'))
-    if randint(0,10)==b: print('\033[32mvoce chutou o numero certo :)')
-    else:
-        print('voce errou')
-        z+=1
-print(f'voce conseguiu com {z} tentativas.')
