13140edf5a0aed7afca38211fc1c34e8ef454ed9
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-09 10:56:16 -0300

    adicionando

--- ex028 e 58.py
+++ ex028 e 58.py
@@ -5,6 +5,8 @@ while b[0]!=b[2]:
     if b[2]>b[0]:
         print('é maior')
         b[1]+=1
+    elif b[0]>10:
+        print('\033[1;31mde 0 a 10\033[m')
     else:
         print('é menor')
         b[1]+=1
