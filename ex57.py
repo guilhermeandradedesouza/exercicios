6f591f1c2a4d897572a51f9de5a74d5baf9323d9
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-09 20:54:57 -0300

    adicionando

--- ex57.py
+++ ex57.py
@@ -1,5 +1,5 @@
-s=input('sexo:')
+s=input('sexo:').upper()
 while s not in 'MF':
     s=input('invalido, digite novamente:').upper()
-if 'M' in s:print('o sexo digitado foi masculino')
+if 'M' in s:print('\no sexo digitado foi masculino')
 else:print('\nsexo feminino')
