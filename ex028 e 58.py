a832c440c196eecc1b12dfc8d3e7363dd16a5631
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-09 10:35:53 -0300

    adicionando

--- ex028 e 58.py
+++ ex028 e 58.py
@@ -2,10 +2,11 @@ from random import randint
 b=[0,0,randint(0,10)]
 b[0]=int(input('numero de 0 a 10:'))
 while b[0]!=b[2]:
-    print('voce errou')
-    if b[2]>b[0]: print('é maior')
+    if b[2]>b[0]:
+        print('é maior')
+        b[1]+=1
     else:
         print('é menor')
         b[1]+=1
-        b[0]=('numero de 0 a 10')
-print(f'voce conseguiu com {b[1]} tentativas.')
+    b[0]=int(input('numero de 0 a 10:'))
+print(f'\nvoce conseguiu com {b[1]} tentativas.')
