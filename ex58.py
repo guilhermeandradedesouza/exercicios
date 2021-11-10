8e0d0bdce33cb08b3dd761e6e0650757259e8055
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-09 21:30:22 -0300

    adicionando

--- ex58.py
+++ ex58.py
@@ -11,4 +11,5 @@ while 'sair' not in s:
         elif n[0]==n[1]: print(f'{n[0]}={n[1]}')
         else: print(f'{n[1]}>{n[0]}')
     else:
-        if 'sair' not in s:print('insira os numeros novamente.')
+        if 'novos' in s:print('insira os numeros novamente.')
+        else:print('\n\033[1;31mopção invalida digite os numeros novamente.\033[m\n')
