e7535947f0d57582d7d8efa958732e1eba9d17a2
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-14 23:52:05 -0300

    modificando

--- ex051 e 61 e 62.py
+++ ex051 e 61 e 62.py
@@ -5,14 +5,14 @@ if g=='for':
     for c in range(b,a*9+b+1,a):
         print(c,end=' ')
 elif g=='while':
-    a=[0,0,0,'sim']
+    a=[0,0,0,'sim',1]
     a[1]=a[0]=int(input('\nprimeiro termo da P.A:'))
     b=int(input('razão da P.A:'))
-    while a[0]!=a[1]+b*(11+a[2]) and a[3]=='sim':
+    while a[0]!=a[1]+b*(11+a[2]) and a[3]=='sim' and a[4]!=0:
         print(a[0],end=' ')
         a[0]+=b
         if a[0]==a[1]+b*(11+a[2]):
             a[3]=input('\nquer mais? ')
             if 'sim' in a[3]:
                 a[0]=a[1]
-                a[2]=int(input(f'\033[1;33mobs:É em relação ao numero {a[1]+b*10}\033[m\nmais quantos? '))
+                a[4]=a[2]=int(input(f'\033[1;33mobs:É em relação ao numero {a[1]+b*10}\033[m\nmais quantos? '))
