933227d85700158042341f43491e8b9bc6d54b84
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-15 17:59:49 -0300

    modificando

--- ex64.py
+++ ex64.py
@@ -4,4 +4,6 @@ while a[1]!=999:
     a[1]=int(input('\033[mnumero:' if a[0]==0 else 'numero:'))
     a[0]+=1
     a[2]+=a[1]
-print(f'\nvoce digitou {a[0]-1} numeros e a soma dos números foi {a[2]-999}.')
+    if a[1]==999:
+        a[2]-=999
+print(f'\nvoce digitou {a[0]-1} numeros e a soma dos números foi {a[2]}.')
