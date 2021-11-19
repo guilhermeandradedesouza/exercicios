979d777761bf4d3b10903f94cfd01bc880809d19
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-19 14:45:57 -0300

    modificANDO 64

--- ex64.py
+++ ex64.py
@@ -2,8 +2,7 @@ a=[0,0,0]
 print('programa de somas\n'+'<'+'='*30+'>'+'\n\033[33mdigite 999 para parar.')
 while a[1]!=999:
     a[1]=int(input('\033[mnumero:' if a[0]==0 else 'numero:'))
+    if a[1]==999:break
     a[0]+=1
     a[2]+=a[1]
-    if a[1]==999:
-        a[2]-=999
 print(f'\nvoce digitou {a[0]-1} numeros e a soma dos números foi {a[2]}.')
