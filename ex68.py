467faccd870fcdb2dcf513e260fac4e3eeca13f9
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-19 23:11:49 -0300

    modificando o 68

--- ex68.py
+++ ex68.py
@@ -5,11 +5,11 @@ while True:
     pm=input('impar ou par? ')
     if n>2:a=randint(0,n)+n
     else:a=randint(0,11)+n
-    if 'par' in pm:pm='par'
-    elif 'impar':pm='impar'
+    if pm=='p':pm='par'
+    elif pm=='i':pm='impar'
     if a%2==0 and pm=='par':pass
     elif a%2!=0 and pm=='impar':pass
     else:break
     g+=1
-    print(f'Voce venceu, {a-n}+{n}={a} é {pm}\n\033[33m{g+1} ROUND\033[m')
-print(f'\033[31m\nVoce perdeu com o total de {g} vitorias' if g>0 else f'\nVoce perdeu')
+    print(f'Voce venceu, {a-n}+{n}={a} é {pm}\n\033[33;1m{g+1} ROUND\033[m')
+print(f'\033[31;1m\nVocê perdeu com o total de {g} vitorias' if g>0 else f'\n\033[31;1mVocê perdeu')
