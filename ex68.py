60a885d0c92744cf892b1384f762194e036ee0d7
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-19 23:02:22 -0300

    modificando o 68

--- ex68.py
+++ ex68.py
@@ -4,10 +4,12 @@ while True:
     n=int(input('jogo do impar ou par\nnumero:'.upper()))
     pm=input('impar ou par? ')
     if n>2:a=randint(0,n)+n
-    else:a=randint(0,11)
-    if pm=='p':pm='par'
-    elif pm=='i':pm='impar'
-    if a%2==0 and 'impar' in pm:break
+    else:a=randint(0,11)+n
+    if 'par' in pm:pm='par'
+    elif 'impar':pm='impar'
+    if a%2==0 and pm=='par':pass
+    elif a%2!=0 and pm=='impar':pass
+    else:break
     g+=1
-    print(f'Voce venceu, {a-n}+{n}={a} é par\n\033[35m{g+1} ROUND\033[m' if pm=='par' and a%2==0 else f'Voce venceu, {a-n}+{n}={a} é {pm}\n\033[35m{g+1} ROUND\033[m')
-print(f'\033[31m\nVoce perdeu com o total de {g} vitorias' if g>0 else '\nVoce perdeu')
+    print(f'Voce venceu, {a-n}+{n}={a} é {pm}\n\033[33m{g+1} ROUND\033[m')
+print(f'\033[31m\nVoce perdeu com o total de {g} vitorias {a}' if g>0 else f'\nVoce perdeu {a}')
