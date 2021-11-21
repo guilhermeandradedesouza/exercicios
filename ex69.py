31df386a452a2ed06ce476966f7a0a06be2887ee
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-21 02:10:16 -0300

    acrescentando informações o 69

--- ex69.py
+++ ex69.py
@@ -1,4 +1,4 @@
-c=a=b=m=f=menordeidade=p=0
+c=a=b=m=f=idademenorque20=maiorque18=p=0
 while True:
     while True:
         a=input('qual seu sexo? ' if a==0 else '\nqual seu sexo? ')
@@ -11,9 +11,10 @@ while True:
     b=int(b)
     if 'f' in a:
         f+=1
-        if b<20:menordeidade+=1
+        if b<20:idademenorque20+=1
+    if b>18:maiorque18+=1
     while True:
         c=input('\nquer continuar? ')
         if c=='sim' or c=='s' or c=='n' or c=='nao' or c=='não':break
     if c=='n' or c=='nao' or c=='não':break
-print(f'\033[1m\npessoas:{p}\ntotal de mulheres:{f}\nmulheres menores de 18:{menordeidade}\nhomens:{m}'.upper())
+print(f'\033[1m\npessoas:{p}\ntotal de mulheres:{f}\nmulheres menores de 20:{idademenorque20}\nhomens:{m}\nmaiores de 18 anos:{maiorque18}'.upper())
