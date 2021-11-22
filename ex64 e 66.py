7c33c67727ff130ccf2eccb0dba0f052143312b2
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-22 14:32:39 -0300

    melhorando o 66

--- ex64 e 66.py
+++ ex64 e 66.py
@@ -1,8 +1,7 @@
 a=[0,0,0]
-print('programa de somas\n'+'<'+'='*30+'>'+'\n\033[33mdigite 999 para parar.')
 while a[1]!=999:
-    a[1]=int(input('\033[mnumero:' if a[0]==0 else 'numero:'))
+    a[1]=int(input('programa de somas\n'+'<'+'='*30+'>'+'\n\033[33mdigite 999 para parar.'+'\n\033[mnumero:' if a[0]==0 else 'numero:'))
     if a[1]==999:break
     a[0]+=1
     a[2]+=a[1]
-print(f'\nvoce digitou {a[0]} numeros e a soma dos números foi {a[2]}.')
+print(f'\nvoce digitou {a[0]} numeros e a soma dos números foi {a[2]}.\n'+'<'+'='*30+'>')
