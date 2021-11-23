1d017c384fcc254214b6229b0bf794752993ba21
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-22 22:14:37 -0300

    comprimindo o 67

--- ex 67.py
+++ ex 67.py
@@ -1,5 +1,4 @@
 while True:
     n=int(input('digite um numero para verificar sua tabuada:'))
     if n<0:break
-    for a in range(1,11):
-        print('<'+'-'*50+'>'+f'\n{n}x{a}={n*a}' if a==1 else f'{n}x{a}={n*a}','\n'+'<'+'-'*50+'>' if a==10 else '')
+    for a in range(1,11):print('<'+'-'*50+'>'+f'\n{n}x{a}={n*a}' if a==1 else f'{n}x{a}={n*a}','\n'+'<'+'-'*50+'>' if a==10 else '')
