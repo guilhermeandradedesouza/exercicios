4cd79929a2d6b85a38cd9841eea304db68fa0147
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-22 23:25:22 -0300

    rescrevendo o 68

--- ex68.py
+++ ex68.py
@@ -2,14 +2,15 @@ from random import randint
 g=0
 while True:
     n=int(input('jogo do impar ou par\nnumero:'.upper()))
-    pm=input('impar ou par? ')
+    pm=input(f'impar ou par? ')
     if n>2:a=randint(0,n)+n
     else:a=randint(0,11)+n
-    if pm=='p':pm='par'
-    elif pm=='i':pm='impar'
-    if a%2==0 and pm=='par':pass
-    elif a%2!=0 and pm=='impar':pass
-    else:break
+    if pm=='p':
+        pm='par'
+        if a%2!=0:break
+    elif pm=='i':
+        pm='impar'
+        if a%2==0:break
     g+=1
     print(f'Voce venceu, {a-n}+{n}={a} é {pm}\n\033[33;1m{g+1} ROUND\033[m')
 print(f'\033[31;1m\nVocê perdeu com o total de {g} vitorias' if g>0 else f'\n\033[31;1mVocê perdeu')
