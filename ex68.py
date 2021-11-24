f42161dfdc54576785bcbaf97061348c97a5dc49
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-23 23:43:46 -0300

    melhorando o 68

--- ex68.py
+++ ex68.py
@@ -1,16 +1,18 @@
 from random import randint
-g=0
+g=r=0
 while True:
-    n=int(input('jogo do impar ou par\nnumero:'.upper()))
-    pm=input(f'impar ou par? ')
+    n=''
+    r=0
+    if g==0:print('jogo do impar ou par\n'.upper()+'\033[1;33m'+'1 Round'.upper()+'\033[m')
+    while n.isnumeric()!=True:
+        n=input('numero:'.upper())
+        if n=='resposta':r=1
+    n=int(n)
     if n>2:a=randint(0,n)+n
     else:a=randint(0,11)+n
-    if pm=='p':
-        pm='par'
-        if a%2!=0:break
-    elif pm=='i':
-        pm='impar'
-        if a%2==0:break
+    pm=input(f'impar ou par? a resposta é {a} ' if r==1 else 'impar ou par? ')
+    if a%2!=0 and pm=='p' or pm=='par':break
+    if a%2==0 and pm=='i' or pm=='impar':break
     g+=1
-    print(f'Voce venceu\ncomputador:{a-n}\njogador:{n}\n{a-n}+{n}={a} é {pm}\n\033[33;1m{g+1} ROUND\033[m')
-print(f'\033[31;1m\nVocê perdeu com o total de {g} vitorias' if g>0 else f'\n\033[31;1mVocê perdeu')
+    print(f'\033[36mVoce venceu\ncomputador:{a-n}\njogador:{n}\n{a-n}+{n}={a} é par\n\033[33;1m{g+1} ROUND\033[m' if a%2==0 else f'\033[36mVoce venceu\ncomputador:{a-n}\njogador:{n}\n{a-n}+{n}={a} é impar\n\033[33;1m{g+1} ROUND\033[m')
+print(f'\033[31;1m\nVocê perdeu com o total de {g} vitorias.' if g>0 and g!=1 else f'\n\033[31;1mVocê perdeu.', f'\033[31;1m\nVocê perdeu, teve uma vitoria.' if g==1 else '')
