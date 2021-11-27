d70421e2a9e8ea512cc8d87d4fe3897650eef78b
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-26 23:05:07 -0300

    reduzindo o 69

--- ex69.py
+++ ex69.py
@@ -13,7 +13,6 @@ while c!='n' and c!='nao' and c!='não':
         f+=1
         if b<20:idademenorque20+=1
     if b>18:maiorque18+=1
-    while True:
-        c=input('\nquer continuar? ')
-        if c[0]=='s' or c[0]=='n':break
+    c = input('\nquer continuar? ').lower()
+    while c[0]!='s' and c[0]!='n':c=input('\nquer continuar? ').lower()
 print(f'\033[1m\npessoas:{p}\ntotal de mulheres:{f}\nmulheres menores de 20:{idademenorque20}\nhomens:{m}\nmaiores de 18 anos:{maiorque18}'.upper())
