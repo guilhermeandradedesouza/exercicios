478b8ed11587dd717e2a199e6203406525d87c7a
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2021-11-27 21:01:53 -0300

    diminuindo o 70

--- ex70.py
+++ ex70.py
@@ -1,10 +1,10 @@
 n=p=mp=c=ptotal=maior1000=nmp=g=0
-while True:
+while c[0]!='n':
+    c=''
     n=input('nome do produto:' if g==0 else '\nnome do produto:')
     if g==0:nmp=n
-    while True:
-        p=input('preço do produto:')
-        if p.isnumeric()==True:break
+    p = input('preço do produto:')
+    while p.isnumeric()!=True:p=input('preço do produto:')
     p=float(p)
     if p>1000:maior1000+=1
     if g==0:mp=p
@@ -13,7 +13,5 @@ while True:
         nmp=n
     ptotal+=p
     g+=1
-    while c!='nao' and c!='não' and c!='n' and c!='sim' and c!='s':c=input('quer continuar? ')
-    if c=='não' or c=='n' or c=='nao':break
-    c=''
+    while c[0]!='n':c=input('quer continuar? ')
 print(f'\ntotal gasto:{ptotal:.2f}R$\nmenor preço:{mp:.2f}R$\nnome do produto de menor preço:{nmp}\nprodutos acima de 1000$:{maior1000}')
