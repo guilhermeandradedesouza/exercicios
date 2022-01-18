d822eb49af20718a4759f194f28836501f206e18
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-01-18 11:17:31 -0300

    :)

--- somasseguidas.py
+++ somasseguidas.py
@@ -1,6 +1,6 @@
 from random import randint
 while True:
     numeros=[randint(1,9),randint(1,9)]
-    resposta=input(f'{numeros[0]}+{numeros[1]}')
+    resposta=int(input(f'{numeros[0]}+{numeros[1]}:'))
     if resposta==numeros[0]+numeros[1]:print('acertou')
-    else:print('\033[31merrou\033[m')
+    else:print(f'\033[31merrou\033[m\na resposta era{numeros[0]+numeros[1]}')
