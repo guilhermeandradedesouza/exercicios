ffca6e341567a570feeea05c801c3825fd01c815
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-01-18 18:48:03 -0300

    72 extra

--- ex72.py
+++ ex72.py
@@ -1,5 +1,8 @@
 numex=('zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
-while True:
-    inteiro=input('numero entre 0 e 20:')
-    if inteiro.isnumeric()==True and 0<=int(inteiro)<=20:break
-print(f'\n{numex[int(inteiro)]}.')
+repitir='s'
+while repitir!='n':
+    while True:
+        inteiro=input('numero entre 0 e 20:')
+        if inteiro.isnumeric() and 0<=int(inteiro)<=20:break
+    print(f'\n{numex[int(inteiro)]}.\n')
+    repitir=input('deseja repitir? ')
