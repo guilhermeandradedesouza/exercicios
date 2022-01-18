2ac05d95e8cae557df872628742de98e266314e9
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-01-18 11:34:17 -0300

    77 funcionando

--- ex77.py
+++ ex77.py
@@ -1,3 +1,9 @@
 palavras=('felicidade','tristeza','amor','vida','familia','verdade','promessa','mentira')
 for a,g in enumerate(palavras):
-    print(f'na palavra {g} temos as vogais {palavras[a].index("a"),palavras[a].index("e"),palavras[a].index("i"),palavras[a].index("o"),palavras[a].index("u")}')
+    print(f'na palavra {g} temos as vogais ',end='')
+    if 'a' in palavras[a]:print('a',end=' ')
+    if 'o' in palavras[a]: print('o', end=' ')
+    if 'i' in palavras[a]: print('i', end=' ')
+    if 'e' in palavras[a]: print('e', end=' ')
+    if 'u' in palavras[a]: print('u', end=' ')
+    print('\n')
