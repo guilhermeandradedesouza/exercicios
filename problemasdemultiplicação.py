6e93be9e226b3531071c037231d84775caa40d15
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-01-17 23:09:50 -0300

    quantidade de acertos

--- problemasdemultiplicação.py
+++ problemasdemultiplicação.py
@@ -3,7 +3,7 @@ n=['a',0]
 repeticoes=int(input('quantas repetições antes de perguntar se voce quer parar? '))
 while n[0][0].lower()!='n':
     for a in range(repeticoes):
-        b=[randint(0,9),randint(0,9)]
+        b=[randint(1,9),randint(1,9)]
         c=input(f'{b[0]}x{b[1]}:'if n[0]=='a' else f'\n{b[0]}x{b[1]}:')
         while c.isnumeric()!=True:input(f'digite novamente\n{b[0]}x{b[1]}:'if n[0]=='a' else f'\ndigite novamente\n{b[0]}x{b[1]}:')
         c=int(c)
@@ -13,3 +13,4 @@ while n[0][0].lower()!='n':
         else:print(f'\033[31mvoce errou\033[m\nresposta:{b[0]*b[1]}\n')
     n[0]=input('deseja continuar?')
     if n[0].isalpha()==False:n[0]='g'
+print(f'voce acertou {n[1]} questões')
