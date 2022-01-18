48f5cf935a40282602061ea99028bd04f5c8651c
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-01-17 23:20:28 -0300

    melhorando

--- problemasdemultiplicação.py
+++ problemasdemultiplicação.py
@@ -1,5 +1,5 @@
 from random import randint
-n=['a',0]
+n=['a',0,0]
 repeticoes=int(input('quantas repetições antes de perguntar se voce quer parar? '))
 while n[0][0].lower()!='n':
     for a in range(repeticoes):
@@ -7,10 +7,11 @@ while n[0][0].lower()!='n':
         c=input(f'{b[0]}x{b[1]}:'if n[0]=='a' else f'\n{b[0]}x{b[1]}:')
         while c.isnumeric()!=True:input(f'digite novamente\n{b[0]}x{b[1]}:'if n[0]=='a' else f'\ndigite novamente\n{b[0]}x{b[1]}:')
         c=int(c)
+        n[2]+=1
         if c==b[0]*b[1]:
             print('\033[32mvoce acertou\033[m\n')
             n[1]+=1
         else:print(f'\033[31mvoce errou\033[m\nresposta:{b[0]*b[1]}\n')
     n[0]=input('deseja continuar?')
     if n[0].isalpha()==False:n[0]='g'
-print(f'voce acertou {n[1]} questões')
+print(f'\n\033[94mVoce acertou {n[1]} questões de {n[2]} ou seja %{n[1]*100/n[2]:.0f}.' if n[1]*100/n[2]>50 else f'\n\033[94mVoce acertou {n[1]} questões de {n[2]} ou seja \033[93m%{n[1]*100/n[2]}.')
