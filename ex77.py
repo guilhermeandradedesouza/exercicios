11c93a946aee23520a4883ffa39ac3f247db68dd
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-01-18 12:52:43 -0300

    77 bom

--- ex77.py
+++ ex77.py
@@ -1,9 +1,7 @@
 palavras=('felicidade','tristeza','amor','vida','familia','verdade','promessa','mentira')
+vogais=('a','e','i','o','u')
 for a,g in enumerate(palavras):
     print(f'na palavra {g} temos as vogais ',end='')
-    if 'a' in palavras[a]:print('a',end=' ')
-    if 'o' in palavras[a]: print('o', end=' ')
-    if 'i' in palavras[a]: print('i', end=' ')
-    if 'e' in palavras[a]: print('e', end=' ')
-    if 'u' in palavras[a]: print('u', end=' ')
+    for c in vogais:
+        if c in palavras[a]:print(c,end=' ')
     print('\n')
