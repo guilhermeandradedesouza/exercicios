b97e9a035313c8f600c396402b436f480762acc9
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-01-20 09:13:00 -0300

    diminuindo

--- ex74.py
+++ ex74.py
@@ -1,3 +1,3 @@
 from random import randint
 a=(randint(0,10),randint(0,10),randint(0,10),randint(0,10),randint(0,10))
-print(f'Os falores sorteados foram:{a}\no menor valor foi {sorted(a)[0]}\nO maior foi {sorted(a)[-1]}')
+print(f'Os falores sorteados foram:{a}\no menor valor foi {min(a)}\nO maior foi {max(a)}')
