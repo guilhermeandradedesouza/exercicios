3312d5f545d23df952ba60cc2cd95144f11fa7eb
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-01-20 09:26:59 -0300

    terminei

--- ex75.py
+++ ex75.py
@@ -1,10 +1,10 @@
 a=[0,0,0,0,0]
 for v in range(4):a[v]=int(input('numero:'))
 valores=(a[0],a[1],a[2],a[3])
-print(f'Os valores digitados foram:{valores}.\nO 9 apereceu {valores.count(9)} vezes.' if valores.count(9)>1 or valores.count(9)==0 else 'O 9 so foi digitado 1 vez.')
+print(f'Os valores digitados foram:{valores}.\nO 9 apareceu {valores.count(9)} vezes.' if valores.count(9)>1 or valores.count(9)==0 else 'O 9 so foi digitado 1 vez.')
 if 3 not in valores:print('o valor 3 não apareceu.')
 else:print(f'O valor 3 foi digitado pela primeira vez na {valores.index(3)+1}º posição.')
-print('os valores pares digitados foram:')
+print('os valores pares digitados foram:',end='')
 for g in valores:
     if g%2==0:
         print(g,end=' ')
