264a7cc906fd668848263ffab077f8e6e446256f
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-01-22 13:23:39 -0300

    deixando bonito

--- ex77.py
+++ ex77.py
@@ -1,7 +1,6 @@
 palavras=('felicidade','tristeza','amor','vida','familia','verdade','promessa','mentira')
 vogais=('a','e','i','o','u')
 for a,g in enumerate(palavras):
-    print(f'na palavra {g} temos as vogais ',end='')
+    print('\033[1m\n'+'_-'*40+f'\nNa palavra {g} temos as vogais ' if a>=1 else f'Na palavra {g} temos as vogais ',end='')
     for c in vogais:
         if c in palavras[a]:print(c,end=' ')
-    print('\n')
