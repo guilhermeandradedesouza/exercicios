bf97d4ad61ca2c48e9ccbdfedf418780bf14ce3b
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-01-27 19:21:14 -0300

    reduzindo

--- problemasdemultiplicacao.py
+++ problemasdemultiplicacao.py
@@ -1,5 +1,5 @@
 from random import randint
-erradas=[0]
+erradas=[]
 n=['a',0,0]
 repeticoes=int(input('quantas repetições antes de perguntar se voce quer parar? '))
 while n[0][0].lower()!='n':
@@ -14,9 +14,8 @@ while n[0][0].lower()!='n':
             n[1]+=1
         else:
             print(f'\033[31mvoce errou\033[m\nresposta:{b[0]*b[1]}\n')
-            erradas.insert(erradas[0]+1,f'{b[0]}x{b[1]}')
-            erradas[0]+=1
+            erradas.insert(a,f'{b[0]}x{b[1]}')
     n[0]=input('deseja continuar?')
     if n[0].isalpha()==False:n[0]='g'
-print(f'\n\033[94mVoce acertou {n[1]} questões de {n[2]} ou seja %{n[1]*100/n[2]:.0f}.\n\033[31mAs questoes erradas foram:'if n[1]*100/n[2]>50 else f'\n\033[94mVoce acertou {n[1]} questões de {n[2]} ou seja \033[93m%{n[1]*100/n[2]}.\n\033[31mAs questoes erradas foram:',end='')
-for abc in erradas[1:]:print(f'{abc}',end=' ')
+print(f'\n\033[94mVoce acertou {n[1]} questões de {n[2]} ou seja %{n[1]*100/n[2]:.0f}.\n\033[31mAs questoes erradas foram:'if n[1]*100/n[2]>50 else f'\n\033[94mVoce acertou {n[1]} questões de {n[2]} ou seja \033[93m%{n[1]*100/n[2]}.\n\033[31mAs questoes erradas foram: ',end='')
+for abc in erradas:print(f'{abc}',end=' ')
