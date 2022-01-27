028e424142b09a82f2983ff3f5be72383a7d5063
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-01-27 19:36:24 -0300

    nada

--- problemasdemultiplicacao.py
+++ problemasdemultiplicacao.py
@@ -14,7 +14,7 @@ while n[0][0].lower()!='n':
             n[1]+=1
         else:
             print(f'\033[31mvoce errou\033[m\nresposta:{b[0]*b[1]}\n')
-            erradas.insert(a,f'{b[0]}x{b[1]}')
+            erradas.append(f'{b[0]}x{b[1]}')
     n[0]=input('deseja continuar?')
     if n[0].isalpha()==False:n[0]='g'
 print(f'\n\033[94mVoce acertou {n[1]} questões de {n[2]} ou seja %{n[1]*100/n[2]:.0f}.\n\033[31mAs questoes erradas foram:'if n[1]*100/n[2]>50 else f'\n\033[94mVoce acertou {n[1]} questões de {n[2]} ou seja \033[93m%{n[1]*100/n[2]}.\n\033[31mAs questoes erradas foram: ',end='')
