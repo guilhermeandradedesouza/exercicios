3f5dbd6199dc1ae9d756a31c123303db874f94b9
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-02-19 12:37:56 -0300

    80 funcionando

--- ex80.py
+++ ex80.py
@@ -1,24 +1,10 @@
 lista=[]
-for a in range(int(input('vezes:'))+1):
-    b=int(input('numero:'))
-    if a==0:
-        lista.append(b)
-        print('add ao final')
-    else:
-        if b>max(lista):
-            lista.append(b)
-            print('add ao final')
-        elif b<min(lista):
-            lista.insert(0,b)
-            print('add casa 0')
-        else:
-            if len(lista)>2:
-                for g in lista[1:-1]:
-                    if b>g:
-                        lista.insert(lista.index(g)+1,b)
-                        print(f'add na casa {lista.index(b)}')
-                        break
-            else:
-                lista.insert(1,b)
-                print('add casa 1')
+for a in range(5):
+    numero=int(input('numero:'))
+    if a==0:lista.append(numero)
+    elif numero>max(lista):lista.append(numero)
+    elif numero<min(lista):lista.insert(0,numero)
+    elif a==2 and max(lista)>numero>min(lista):lista.insert(1,numero)
+    elif a==3:lista.insert(1 if lista[1]>numero else 2,numero)
+    elif a==4 and max(lista[1:-1])>numero>min(lista[1:-1]):lista.insert(2,numero)
 print(lista)
