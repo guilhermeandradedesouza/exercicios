eca5d7ce73d8224dc24ece34603b5a19b37f9be4
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-02-20 14:26:40 -0300

    é

--- ex80.py
+++ ex80.py
@@ -1,11 +1,1 @@
-lista=[]
-for a in range(5):
-    numero=int(input('numero:'))
-    if a==0:lista.append(numero)
-    elif numero>max(lista):lista.append(numero)
-    elif numero<min(lista):lista.insert(0,numero)
-    elif a==2 and max(lista)>numero>min(lista):lista.insert(1,numero)
-    elif a==3:lista.insert(1 if lista[1]>numero else 2,numero)
-    elif a==4 and max(lista[1:-1])>numero>min(lista[1:-1]):lista.insert(2,numero)
-    print(f'posição {lista.index(numero)}.')
-print(lista)
+#não preciso usar se tem o sorted
