192319d5ca1096a8ea9ff8d9b165bb1c092733dd
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-02-22 14:26:13 -0300

    nada

--- ex83.py
+++ ex83.py
@@ -1,6 +1,3 @@
-'''a=input('equação:')
-if (a.count('(')+a.count(')'))%2==0:print('a expreção é verdadeira.')
-else:print('falsa')'''
 a=input('equação:')
-print(a.split(')'))
-for c in a.split(')'):
+if (a.count('(')+a.count(')'))%2==0:print('a expreção é verdadeira.')
+else:print('falsa')
