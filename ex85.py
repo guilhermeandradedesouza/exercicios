ee102d4977e31c0557d2cd13c70ab78e49608c6d
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-02-24 13:50:07 -0300

    sorted

--- ex85.py
+++ ex85.py
@@ -3,4 +3,4 @@ for c in range(7):
     numero=int(input('numero:'))
     if numero%2==0:valores[0].append(numero)
     else:valores[1].append(numero)
-for c in range(2):print('\nvalores','impares:' if c==1 else 'pares:' ,f'{valores[0 if c==0 else 1]}')
+for c in range(2):print('\nvalores','impares:' if c==1 else 'pares:' ,f'{sorted(valores[0 if c==0 else 1])}')
