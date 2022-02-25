66654a25d9fbd3ade4c20ba44d33717f4b9407fd
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-02-25 01:35:38 -0300

    melhorando

--- ex83.py
+++ ex83.py
@@ -1,4 +1,7 @@
+quant=0
 a=input('equação:')
-if a[:a.index(')')+1].count('(')==0:print('falso'),exit()
+for num,letras in enumerate(a):
+    if letras==')':quant+=1
+    if letras==')' and a[:num].count('(')==quant:print('falso'),exit()
 if (a.count('(')+a.count(')'))%2==0:print('a expreção é verdadeira.')
 else:print('falsa')
