407d24e111f79cb360fe85ae2bbbf3e954691c9f
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-03-15 13:10:29 -0300

    informaçoes

---
+++ fstrings.py
@@ -0,0 +1,5 @@
+print(f'{"qualquer coisa": ^30}')#no centro
+print(f'{"qualquer coisa": <30}')#no canto
+print(f'{"qualquer coisa": >30}')#no outro
+print(f'{12:0%}')#o % multiplica o valor por 100 e inclui o % ao final
+print(f'{1:.10f}')#O .xf diz que a string resultante deve ter x casas decimais
