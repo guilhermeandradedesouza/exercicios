ad80859dce3decacd505d3cd5bc7b571291dca58
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-03-29 22:35:54 -0300

    interpretei errado *correção*

--- ex88.py
+++ ex88.py
@@ -1,9 +1,12 @@
 from random import randint
 from time import sleep
-print('\033[1m-==-'*8+'\njogo da mega sena\n'.upper()+'-==-'*8+'\033[m')
+sorteio=randint(1,60)
+print('\033[1m-==-'*8+f'\n{"jogo da mega sena":^32}\n'.upper()+'-==-'*8+'\033[m')
 lista=[[] for c in range(int(input('quantidade de jogos:')))]
 for a in range(len(lista)):
-    for c in range(int(input(f'Quantidade de numeros para serem sorteados no jogo {a+1}:'))):lista[a].append(randint(1,60))
+    for c in range(int(input(f'Quantidade de numeros para serem sorteados no jogo {a+1}:'))):
+        while sorteio in lista[a]:sorteio=randint(1,60)
+        lista[a].append(sorteio)
 for num,c in enumerate(lista):
     print('\n' if num==0 else '',f'jogo {num+1}: {lista[num]}')
     sleep(1)
