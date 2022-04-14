ff3f0546f12fa62f0d3b53159852b877c2ed72ee
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-04-13 21:39:25 -0300

    nada

--- ex91.py
+++ ex91.py
@@ -2,7 +2,6 @@ from random import randint
 from time import sleep
 lugar=1
 jogadores={f'jogador {num}':randint(1,6) for num in range(1,5)}
-print(sorted(jogadores.values()))
 print('\033[1mValores:\033[m')
 for jogador,valor in jogadores.items():
     print(f'{jogador} tirou {valor}')
