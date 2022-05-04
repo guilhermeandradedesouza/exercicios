f34668b5101f26da9a82df96f27e49bf668f4543
guilhermeandradedesouza <93688314+guilhermeandradedesouza@users.noreply.github.com>
2022-05-03 21:37:43 -0300

    correções

--- ex95.py
+++ ex95.py
@@ -1,7 +1,15 @@
 jogadores=[]
 for partidas in range(int(input('quantidade de jogadores:'))):
-    jogadores.append({'nome':input('nome do jogador:'),'partidas':int(input('quantidade de partidas:'))})
-    jogadores[partidas]['gols']=[int(input(f'gols na {pontos+1}º partida:')) for pontos in range(jogadores[partidas]['partidas'])]
+    jogadores.append({partidas: {'nome': input('nome do jogador:'), 'partidas': int(input('quantidade de partidas:'))}})
+    jogadores[partidas][partidas]['gols']=[int(input(f'gols na {pontos+1}º partida:')) for pontos in range(jogadores[partidas][partidas]['partidas'])]
     print('-_-'*30)
 print('cod nome     gols        total','-=-'*30,sep='\n')
-for cod,pessoa in enumerate(jogadores):print(f'{cod}  {pessoa["nome"]}      {pessoa["gols"]}        {sum(pessoa["gols"]):<{len(pessoa["gols"])*5}}')
+for cod,pessoa in enumerate(jogadores):print(f'{cod}  {pessoa[cod]["nome"]}      {pessoa[cod]["gols"]}        {sum(pessoa[cod]["gols"]):<{len(pessoa[cod]["gols"])*5}}')
+selecionado=int(input('codigo do jogador:'))
+if selecionado!=999:
+    print()
+    for num in range(len(jogadores)):
+        if selecionado==num:
+            for infos,x in jogadores[num][num].items():print(f'{infos}={x}')
+elif selecionado>len(jogadores):print('\nERRO JOGADOR NÃO EXISTE.')
+else:print('\nOK.')
